67bd0f30546e1b4dad8eef87dea5c30452e9d704
kimjieun0301 <62086003+kimjieun0301@users.noreply.github.com>
2023-07-10 06:06:46 +0900

    chatServer

---
+++ chatServer/chatServer/FrmServer.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace chatServer
+{
+    partial class FrmServer
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnListen = new System.Windows.Forms.Button();
+            this.LvChkServer = new System.Windows.Forms.ListBox();
+            this.Txtcount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnListen
+            // 
+            this.BtnListen.Location = new System.Drawing.Point(12, 12);
+            this.BtnListen.Name = "BtnListen";
+            this.BtnListen.Size = new System.Drawing.Size(127, 57);
+            this.BtnListen.TabIndex = 0;
+            this.BtnListen.Text = "연결";
+            this.BtnListen.UseVisualStyleBackColor = true;
+            this.BtnListen.Click += new System.EventHandler(this.BtnListen_Click);
+            // 
+            // LvChkServer
+            // 
+            this.LvChkServer.FormattingEnabled = true;
+            this.LvChkServer.ItemHeight = 15;
+            this.LvChkServer.Location = new System.Drawing.Point(169, 12);
+            this.LvChkServer.Name = "LvChkServer";
+            this.LvChkServer.Size = new System.Drawing.Size(324, 499);
+            this.LvChkServer.TabIndex = 1;
+            // 
+            // Txtcount
+            // 
+            this.Txtcount.AutoSize = true;
+            this.Txtcount.Location = new System.Drawing.Point(13, 83);
+            this.Txtcount.Name = "Txtcount";
+            this.Txtcount.Size = new System.Drawing.Size(44, 15);
+            this.Txtcount.TabIndex = 2;
+            this.Txtcount.Text = "count";
+            // 
+            // FrmServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 520);
+            this.Controls.Add(this.Txtcount);
+            this.Controls.Add(this.LvChkServer);
+            this.Controls.Add(this.BtnListen);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmServer";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "서버";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnListen;
+        private System.Windows.Forms.ListBox LvChkServer;
+        private System.Windows.Forms.Label Txtcount;
+    }
+}
+
---
+++ chatServer/chatServer/FrmServer.cs
@@ -0,0 +1,113 @@
+﻿using chattingLib;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace chatServer
+{
+    public partial class FrmServer : Form
+    {
+        #region init
+        private TcpListener _listener;
+        bool initialFlag = true;
+        string receivedPath = string.Empty;
+        enum DataPacketType { TEXT = 1, IMAGE };
+        //int dataType = 0;
+        string textData = string.Empty;
+        public FrmServer()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region 채팅 서버 시작
+        private async void BtnListen_Click(object sender, EventArgs e)
+        {
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8080);
+            _listener.Start();
+            initialFlag = true;
+            while (true)
+            {
+                TcpClient client = await _listener.AcceptTcpClientAsync();
+
+                _ = HandleClient(client);
+            }
+        }
+        #endregion
+
+        #region 클라이언트 핸들러
+        public async Task HandleClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+
+            int fileNameLen = 0;
+            byte[] sizeBuffer = new byte[4];
+            byte[] statebuffer = new byte[3];
+            byte[] imgbuffer = new byte[4096];
+            int read;
+            while (true)
+            {
+                read = await stream.ReadAsync(statebuffer, 0, statebuffer.Length);
+                //dataType = BitConverter.ToInt32(statebuffer, 0);
+                String dataType = Encoding.UTF8.GetString(statebuffer, 0, read);
+                if (initialFlag)
+                {
+                    if (Equals(dataType, "img"))
+                    {
+                        read = await stream.ReadAsync(imgbuffer, 0, imgbuffer.Length);
+                        _ = BitConverter.ToInt32(imgbuffer, 0);
+                        fileNameLen = BitConverter.ToInt32(imgbuffer, 0);
+                        string fileName = Encoding.UTF8.GetString(imgbuffer, 4, fileNameLen);
+
+                        string pathUser = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                        string pathDownload = Path.Combine(pathUser, "Downloads");
+
+                        receivedPath = Path.Combine(pathDownload, fileName);
+
+                        if (File.Exists(receivedPath))
+                            File.Delete(receivedPath);
+                    }
+                    else if (Equals(dataType, "txt"))// (dataType == (int)DataPacketType.TEXT)
+                    {
+                        read = await stream.ReadAsync(sizeBuffer, 0, sizeBuffer.Length);
+                        if (read <= 0)
+                            break;
+                        int size = BitConverter.ToInt32(sizeBuffer, 0);
+                        byte[] buffer = new byte[size];
+
+                        read = await stream.ReadAsync(buffer, 0, buffer.Length);
+                        if (read <= 0)
+                            break;
+                        String message = Encoding.UTF8.GetString(buffer, 0, read);
+
+                        var chatForm = CsChatting.Parse(message);
+                        LvChkServer.Items.Add($"{chatForm.MemId},{chatForm.RoomId},{chatForm.MemName},{chatForm.Message}");
+
+                        Txtcount.Text = LvChkServer.Items.Count.ToString();
+
+                        var messageBuffer = Encoding.UTF8.GetBytes($"Server : {message}");
+                        stream.Write(messageBuffer, 0, messageBuffer.Length);
+                    }
+
+                }
+                if (Equals(dataType, "img"))// (dataType == (int)DataPacketType.IMAGE)
+                {
+                    read = await stream.ReadAsync(sizeBuffer, 0, sizeBuffer.Length);
+                    int bytesRead = 107070;//BitConverter.ToInt32(sizeBuffer, 0);
+                    //int bytesRead = await client.GetStream().ReadAsync(sizeBuffer, 0, sizeBuffer.Length);
+                    BinaryWriter bw = new BinaryWriter(File.Open(receivedPath, FileMode.Append));
+                    if (initialFlag)
+                        bw.Write(imgbuffer, 4 + fileNameLen, bytesRead - (4 + fileNameLen));
+                    else
+                        bw.Write(imgbuffer, 0, bytesRead);
+
+                    initialFlag = false;
+                    bw.Close();
+                    await stream.ReadAsync(imgbuffer, 0, bytesRead); //(statebuffer, 0, bytesRead, 0, new AsyncCallback(HandleClient), state);
+                }
+
+            }
+        }
+        #endregion
+    }
+}
